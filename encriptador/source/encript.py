8ae365a7c475cd5e6c08196c4593fad689e13a50
kramos966 <kramos966@gmail.com>
2017-12-04 21:29:38 +0100

    pequeña actualizacion

--- encriptador/source/encript.py
+++ encriptador/source/encript.py
@@ -6,8 +6,8 @@
 
 #kek
 def rot13(text):
+	"""Encriptador de rotacion en 13"""
 	texto_encriptado = ""
-#	alfabeto = [chr(x) for x in range(ord("a"), ord("z")+1)]
 	alfabeto = range(0, 27)
 	for c in text:
 		posicion = ord(c)-ord("a")
@@ -15,6 +15,21 @@ def rot13(text):
 		car_final = posicion2+ord("a")
 		texto_encriptado += chr(car_final)
 	return texto_encriptado
+
+def rotn(text, n):
+	"""Encriptador de rotacion en n. Sustituye la letra por la que esta
+	13 posiciones en adelante. Si n=13, el encriptador funciona como
+	desencriptador"""
+	texto_encriptado = ""
+	alfabeto = range(0, 27)
+	for c in text:
+		posicion = ord(c)-ord("a")
+		posicion2 = (posicion + n)%26
+		car_final = posicion2+ord("a")
+		texto_encriptado += chr(car_final)
+	return texto_encriptado
+
+	
 
 print(rot13(input()))
 
