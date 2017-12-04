fc5105be59f4e4db50570491200c5fbb1d39406a
kramos966 <kramos966@gmail.com>
2017-12-04 20:59:34 +0100

    primer programa

--- encriptador/source/encript.py
+++ encriptador/source/encript.py
@@ -5,3 +5,16 @@
 """
 
 #kek
+def rot13(text):
+	texto_encriptado = ""
+#	alfabeto = [chr(x) for x in range(ord("a"), ord("z")+1)]
+	alfabeto = range(0, 27)
+	for c in text:
+		posicion = ord(c)-ord("a")
+		posicion2 = (posicion + 13)%26
+		car_final = posicion2+ord("a")
+		texto_encriptado += chr(car_final)
+	return texto_encriptado
+
+print(rot13(input()))
+
